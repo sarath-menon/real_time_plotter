408120c718d4fb90be07af7d732df2509bd6d87e
sarath-menon <sarathmenon.downey@gmail.com>
2021-10-31 23:47:22 +0100

    better looking plots

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,8 +12,8 @@ MainWindow::MainWindow(QWidget *parent)
   ui->z_plot->addGraph();
 
   ui->x_plot->graph(0)->setPen(QPen(QColor(255, 255, 255)));
-  ui->y_plot->graph(0)->setPen(QPen(QColor(40, 110, 255)));
-  ui->z_plot->graph(0)->setPen(QPen(QColor(40, 110, 255)));
+  ui->y_plot->graph(0)->setPen(QPen(QColor(255, 255, 255)));
+  ui->z_plot->graph(0)->setPen(QPen(QColor(255, 255, 255)));
 
   QSharedPointer<QCPAxisTickerTime> timeTicker(new QCPAxisTickerTime);
   timeTicker->setTimeFormat("%h:%m:%s");
@@ -35,20 +35,28 @@ MainWindow::MainWindow(QWidget *parent)
   ui->y_plot->yAxis->setLabel("y_position");
   ui->z_plot->yAxis->setLabel("z_position");
 
-  ui->x_plot->xAxis->setLabel("time");
-  ui->y_plot->xAxis->setLabel("time");
-  ui->z_plot->xAxis->setLabel("time");
+  //   ui->x_plot->xAxis->setLabel("time");
+  //   ui->y_plot->xAxis->setLabel("time");
+  //   ui->z_plot->xAxis->setLabel("time");
 
-  // Make plt look nicer
-  // move bars above graphs and grid below bars:
-  ui->x_plot->addLayer("abovemain", ui->x_plot->layer("main"),
-                       QCustomPlot::limAbove);
-  ui->x_plot->addLayer("belowmain", ui->x_plot->layer("main"),
-                       QCustomPlot::limBelow);
-  ui->x_plot->graph(0)->setLayer("abovemain");
-  ui->x_plot->xAxis->grid()->setLayer("belowmain");
-  ui->x_plot->yAxis->grid()->setLayer("belowmain");
+  ui->x_plot->yAxis->setLabelColor(QColor(255, 255, 255));
+  ui->y_plot->yAxis->setLabelColor(QColor(255, 255, 255));
+  ui->z_plot->yAxis->setLabelColor(QColor(255, 255, 255));
 
+  // Some settings to make plots nicer
+  QLinearGradient plotGradient;
+  plotGradient.setStart(0, 0);
+  plotGradient.setFinalStop(0, 350);
+  plotGradient.setColorAt(0, QColor(80, 80, 80));
+  plotGradient.setColorAt(1, QColor(50, 50, 50));
+
+  QLinearGradient axisRectGradient;
+  axisRectGradient.setStart(0, 0);
+  axisRectGradient.setFinalStop(0, 350);
+  axisRectGradient.setColorAt(0, QColor(80, 80, 80));
+  axisRectGradient.setColorAt(1, QColor(30, 30, 30));
+
+  // Set properties
   ui->x_plot->xAxis->setBasePen(QPen(Qt::white, 1));
   ui->x_plot->yAxis->setBasePen(QPen(Qt::white, 1));
   ui->x_plot->xAxis->setTickPen(QPen(Qt::white, 1));
@@ -71,18 +79,58 @@ MainWindow::MainWindow(QWidget *parent)
   ui->x_plot->yAxis->grid()->setZeroLinePen(Qt::NoPen);
   ui->x_plot->xAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
   ui->x_plot->yAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
-  QLinearGradient plotGradient;
-  plotGradient.setStart(0, 0);
-  plotGradient.setFinalStop(0, 350);
-  plotGradient.setColorAt(0, QColor(80, 80, 80));
-  plotGradient.setColorAt(1, QColor(50, 50, 50));
   ui->x_plot->setBackground(plotGradient);
-  QLinearGradient axisRectGradient;
-  axisRectGradient.setStart(0, 0);
-  axisRectGradient.setFinalStop(0, 350);
-  axisRectGradient.setColorAt(0, QColor(80, 80, 80));
-  axisRectGradient.setColorAt(1, QColor(30, 30, 30));
   ui->x_plot->axisRect()->setBackground(axisRectGradient);
+
+  ui->y_plot->xAxis->setBasePen(QPen(Qt::white, 1));
+  ui->y_plot->yAxis->setBasePen(QPen(Qt::white, 1));
+  ui->y_plot->xAxis->setTickPen(QPen(Qt::white, 1));
+  ui->y_plot->yAxis->setTickPen(QPen(Qt::white, 1));
+  ui->y_plot->xAxis->setSubTickPen(QPen(Qt::white, 1));
+  ui->y_plot->yAxis->setSubTickPen(QPen(Qt::white, 1));
+  ui->y_plot->xAxis->setTickLabelColor(Qt::white);
+  ui->y_plot->yAxis->setTickLabelColor(Qt::white);
+  ui->y_plot->xAxis->grid()->setPen(
+      QPen(QColor(140, 140, 140), 1, Qt::DotLine));
+  ui->y_plot->yAxis->grid()->setPen(
+      QPen(QColor(140, 140, 140), 1, Qt::DotLine));
+  ui->y_plot->xAxis->grid()->setSubGridPen(
+      QPen(QColor(80, 80, 80), 1, Qt::DotLine));
+  ui->y_plot->yAxis->grid()->setSubGridPen(
+      QPen(QColor(80, 80, 80), 1, Qt::DotLine));
+  ui->y_plot->xAxis->grid()->setSubGridVisible(true);
+  ui->y_plot->yAxis->grid()->setSubGridVisible(true);
+  ui->y_plot->xAxis->grid()->setZeroLinePen(Qt::NoPen);
+  ui->y_plot->yAxis->grid()->setZeroLinePen(Qt::NoPen);
+  ui->y_plot->xAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
+  ui->y_plot->yAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
+  ui->y_plot->setBackground(plotGradient);
+  ui->y_plot->axisRect()->setBackground(axisRectGradient);
+
+  ui->z_plot->xAxis->setBasePen(QPen(Qt::white, 1));
+  ui->z_plot->yAxis->setBasePen(QPen(Qt::white, 1));
+  ui->z_plot->xAxis->setTickPen(QPen(Qt::white, 1));
+  ui->z_plot->yAxis->setTickPen(QPen(Qt::white, 1));
+  ui->z_plot->xAxis->setSubTickPen(QPen(Qt::white, 1));
+  ui->z_plot->yAxis->setSubTickPen(QPen(Qt::white, 1));
+  ui->z_plot->xAxis->setTickLabelColor(Qt::white);
+  ui->z_plot->yAxis->setTickLabelColor(Qt::white);
+  ui->z_plot->xAxis->grid()->setPen(
+      QPen(QColor(140, 140, 140), 1, Qt::DotLine));
+  ui->z_plot->yAxis->grid()->setPen(
+      QPen(QColor(140, 140, 140), 1, Qt::DotLine));
+  ui->z_plot->xAxis->grid()->setSubGridPen(
+      QPen(QColor(80, 80, 80), 1, Qt::DotLine));
+  ui->z_plot->yAxis->grid()->setSubGridPen(
+      QPen(QColor(80, 80, 80), 1, Qt::DotLine));
+  ui->z_plot->xAxis->grid()->setSubGridVisible(true);
+  ui->z_plot->yAxis->grid()->setSubGridVisible(true);
+  ui->z_plot->xAxis->grid()->setZeroLinePen(Qt::NoPen);
+  ui->z_plot->yAxis->grid()->setZeroLinePen(Qt::NoPen);
+  ui->z_plot->xAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
+  ui->z_plot->yAxis->setUpperEnding(QCPLineEnding::esSpikeArrow);
+  ui->z_plot->setBackground(plotGradient);
+  ui->z_plot->axisRect()->setBackground(axisRectGradient);
 
   obj = std::make_unique<fastdds_thread>(ui->x_plot, ui->y_plot, ui->z_plot);
   obj->start();
